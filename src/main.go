fd97c813087d820b0f0efd9eaf6743b3db98371a
shwatanap <watanabeshou1520@gmail.com>
2023-04-08 17:37:53 +0900

    :sparkles: godotenvで環境変数読み込み

--- src/main.go
+++ src/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/shwatanap/workout-wizard-api/src/config"
 	"github.com/shwatanap/workout-wizard-api/src/infra/db/driver"
 	"github.com/shwatanap/workout-wizard-api/src/presen/router"
 	"github.com/shwatanap/workout-wizard-api/src/wire"
 )
 
 func main() {
+	config.LoadEnv()
 	db := driver.NewDriver()
 	menuHandler := wire.InitMenuHandler(db)
 
